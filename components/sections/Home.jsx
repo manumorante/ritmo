2ad678ba0f1b1efdb9abfce863d07d4f62a13fc7
Manu <manu@estadologico.com>
2023-04-15 07:06:50 +0200

    floating stones

--- components/sections/Home.jsx
+++ components/sections/Home.jsx
@@ -22,6 +22,24 @@ export default function Home() {
   const dateStyle = yxz(percentMinMax(scroll, 0, 350))
   const imageStyle = yxz(percentMinMax(scroll, 0, 500))
 
+  // Stones
+  const pCx = cx('absolute w-4 h-4')
+  const p1Cx = cx('P1', pCx, 'left-10')
+  const p2Cx = cx('P2', pCx, 'left-16')
+  const p3Cx = cx('P3', pCx, 'left-4')
+  const p4Cx = cx('P4', pCx, 'left-2')
+  const p5Cx = cx('P5', pCx, 'left-10')
+  const p6Cx = cx('P6', pCx, 'left-16')
+  const p7Cx = cx('P7', pCx, 'left-4')
+
+  const p1Style = { transform: `translateY(${scroll * -1}px)`, top: '70%' }
+  const p2Style = { transform: `translateY(${scroll * -2}px)`, top: '65%' }
+  const p3Style = { transform: `translateY(${scroll * -3}px)`, top: '60%' }
+  const p4Style = { transform: `translateY(${scroll * -4}px)`, top: '55%' }
+  const p5Style = { transform: `translateY(${scroll * -5}px)`, top: '50%' }
+  const p6Style = { transform: `translateY(${scroll * -6}px)`, top: '55%' }
+  const p7Style = { transform: `translateY(${scroll * -7}px)`, top: '40%' }
+
   return (
     <section ref={ref} className={sectionCx}>
       <div className={logoCx} style={logoStyle}>
@@ -36,7 +54,7 @@ export default function Home() {
       <div className={dateCx} style={dateStyle}>
         <p className={dateTxtCx}>5-6 MAYO</p>
       </div>
-      {/* <code>scroll: {scroll}</code> */}
+
       <Image
         className={imageCx}
         style={imageStyle}
@@ -44,6 +62,63 @@ export default function Home() {
         width={768}
         height={814}
         alt='FX'
+      />
+
+      <Image
+        style={p1Style}
+        className={p1Cx}
+        src={'/stones/p1.png'}
+        width={16}
+        height={16}
+        alt='Stone'
+      />
+      <Image
+        style={p2Style}
+        className={p2Cx}
+        src={'/stones/p2.png'}
+        width={16}
+        height={16}
+        alt='Stone'
+      />
+      <Image
+        style={p3Style}
+        className={p3Cx}
+        src={'/stones/p3.png'}
+        width={16}
+        height={16}
+        alt='Stone'
+      />
+      <Image
+        style={p4Style}
+        className={p4Cx}
+        src={'/stones/p4.png'}
+        width={16}
+        height={16}
+        alt='Stone'
+      />
+      <Image
+        style={p5Style}
+        className={p5Cx}
+        src={'/stones/p5.png'}
+        width={16}
+        height={16}
+        alt='Stone'
+      />
+      <Image
+        style={p6Style}
+        className={p6Cx}
+        src={'/stones/p6.png'}
+        width={16}
+        height={16}
+        alt='Stone'
+      />
+      <Image
+        style={p7Style}
+        className={p7Cx}
+        src={'/stones/p7.png'}
+        width={16}
+        height={16}
+        alt='Stone'
       />
     </section>
   )
