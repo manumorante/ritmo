c9e076ec0b6b8e7e7bf685c3e4a7ea3902b4ebb3
Manu <manu@estadologico.com>
2023-04-15 06:39:40 +0200

    location

--- components/sections/Location.jsx
+++ components/sections/Location.jsx
@@ -11,18 +11,30 @@ export default function Location() {
   const ref = useRef()
   const { scroll } = useParallax({ ref })
 
-  const mainCx = cx('Location section h-[50vh]')
-  const imageCx = cx('w-full h-full object-cover')
-  const imageStyle = yxz(percentMinMax(scroll, 40, 60))
+  const sectionCx = cx('Location section min-h-[60vh]')
+  const imageCx = cx('w-screen h-[30vh] object-cover my-10')
+  const image1Cx = cx(imageCx)
+  const image2Cx = cx(imageCx)
+  const image1Style = yxz(0, scroll * 0.5)
+  const image2Style = yxz(0, scroll * -1.5)
 
   return (
-    <section ref={ref} className={mainCx}>
+    <section ref={ref} className={sectionCx}>
       <Image
-        className={imageCx}
-        style={imageStyle}
+        className={image2Cx}
+        style={image2Style}
         src={'/ermita2.jpg'}
         width={828}
         height={593}
+        alt='Ermita de los Tres Juanes'
+      />
+
+      <Image
+        className={image1Cx}
+        style={image1Style}
+        src={'/ermita.jpg'}
+        width={752}
+        height={430}
         alt='Ermita de los Tres Juanes'
       />
     </section>
